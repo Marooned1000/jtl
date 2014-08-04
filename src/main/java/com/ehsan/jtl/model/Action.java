3b486fa62a09dfd3a9da95f878e4309a49ada8c5
Ehsan Khosrowshahi Asl <ehkasl@gmail.com>
2014-08-04 15:26:42 -0700

    working on translation

---
+++ src/main/java/com/ehsan/jtl/model/Action.java
@@ -0,0 +1,20 @@
+package com.ehsan.jtl.model;
+
+public class Action {
+	String name;
+	String type;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
