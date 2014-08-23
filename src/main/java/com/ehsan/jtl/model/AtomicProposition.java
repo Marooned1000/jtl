0a0c0a16c2116c9c28ad4b0aa0adab90d4b3a336
Marooned202 <maroonedsc@gmail.com>
2014-08-23 14:26:12 -0400

    Atomic proposition

---
+++ src/main/java/com/ehsan/jtl/model/AtomicProposition.java
@@ -0,0 +1,27 @@
+package com.ehsan.jtl.model;
+
+public class AtomicProposition {
+	String moduleAtomicProposition;
+	String argumentAtomicProposition;
+	State stateAtomicProposition;
+	
+	public String getModuleAtomicProposition() {
+		return moduleAtomicProposition;
+	}
+	public void setModuleAtomicProposition(String moduleAtomicProposition) {
+		this.moduleAtomicProposition = moduleAtomicProposition;
+	}
+	public String getArgumentAtomicProposition() {
+		return argumentAtomicProposition;
+	}
+	public void setArgumentAtomicProposition(String argumentAtomicProposition) {
+		this.argumentAtomicProposition = argumentAtomicProposition;
+	}
+	public State getStateAtomicProposition() {
+		return stateAtomicProposition;
+	}
+	public void setStateAtomicProposition(State stateAtomicProposition) {
+		this.stateAtomicProposition = stateAtomicProposition;
+	}	
+	
+}
